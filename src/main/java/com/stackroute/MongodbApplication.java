fbf0ab68c1dbb6b2d272c73c6856bdcd4817b171
priyanka <priyanka150skct@gmail.com>
2019-07-30 19:40:57 +0530

    completed docker exercise2 using mongodb

---
+++ src/main/java/com/stackroute/MongodbApplication.java
@@ -0,0 +1,15 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan
+public class MongodbApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MongodbApplication.class, args);
+	}
+
+}
